686ee8bd024013dbff6cdb8badc40bfe1d5ba097
Tripp Hammond <tripphamm@gmail.com>
2019-06-14 10:57:30 -0400

    Add last-minute stuff; prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 /* eslint-disable react/jsx-curly-brace-presence */
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React from "react";
+import { hot } from "react-hot-loader";
 
-import Reveal from '../custom_modules/reveal/js/reveal';
-import '../custom_modules/reveal/css/reveal.css';
-import '../custom_modules/reveal/css/theme/black.css';
+import Reveal from "../custom_modules/reveal/js/reveal";
+import "../custom_modules/reveal/css/reveal.css";
+import "../custom_modules/reveal/css/theme/black.css";
 
-import Slide from './components/Slide';
-import Code from './components/Code';
-import Emoji from './components/Emoji';
-import ListWithEmojis from './components/ListWithEmojis';
+import Slide from "./components/Slide";
+import Code from "./components/Code";
+import Emoji from "./components/Emoji";
+import ListWithEmojis from "./components/ListWithEmojis";
 
-import reactUILibraryLogo from './images/react-ui-library.png';
-import vpLogo from './images/vp.png';
-import twitterLogo from './images/twitter.png';
-import gitHubLogo from './images/github.png';
+import reactUILibraryLogo from "./images/react-ui-library.png";
+import vpLogo from "./images/vp.png";
+import twitterLogo from "./images/twitter.png";
+import gitHubLogo from "./images/github.png";
 
 class App extends React.Component {
   componentDidMount() {
@@ -30,7 +30,7 @@ class App extends React.Component {
               className="plain"
               style={{
                 height: 200,
-                marginBottom: 50,
+                marginBottom: 50
               }}
               src={vpLogo}
               alt="UI Library"
@@ -38,17 +38,51 @@ class App extends React.Component {
             <h2>UI Team</h2>
           </Slide>
           <Slide>
-            <h3><Emoji unicodeOrShortName=":man_raising_hand:" /></h3>
+            <h3>
+              <Emoji unicodeOrShortName=":man_raising_hand:" />
+            </h3>
             <p className="fragment fade-in">EJ Hammond</p>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <p className="fragment fade-in" style={{ marginRight: 20 }}>Edward James</p>
-              <span><Emoji className="fragment fade-in" unicodeOrShortName=":three:" /></span>
+            <p className="fragment fade-in" style={{ opacity: 0.6 }}>
+              (Front End Developer)
+            </p>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <p className="fragment fade-in" style={{ marginRight: 20 }}>
+                Edward James Hammond
+              </p>
+              <span>
+                <Emoji
+                  className="fragment fade-in"
+                  unicodeOrShortName=":three:"
+                />
+              </span>
             </div>
-            <div className="fragment fade-in" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <img className="plain" style={{ height: 60 }} src={twitterLogo} alt="twitter" />
-              <img className="plain" style={{ height: 60 }} src={gitHubLogo} alt="twitter" />
+            <div
+              className="fragment fade-in"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <img
+                className="plain"
+                style={{ height: 60 }}
+                src={twitterLogo}
+                alt="twitter"
+              />
+              <img
+                className="plain"
+                style={{ height: 60 }}
+                src={gitHubLogo}
+                alt="twitter"
+              />
               <p>@tripphamm</p>
-
             </div>
           </Slide>
           <Slide>
@@ -56,7 +90,7 @@ class App extends React.Component {
               className="plain"
               style={{
                 height: 200,
-                marginBottom: 50,
+                marginBottom: 50
               }}
               src={reactUILibraryLogo}
               alt="React UI Library"
@@ -64,44 +98,70 @@ class App extends React.Component {
             <h2>React VP UI Library</h2>
           </Slide>
           <Slide>
-            <h3><Emoji unicodeOrShortName=":hockey:" /><Emoji unicodeOrShortName=":goal:" /> Goals</h3>
-            <p className="fragment fade-in">Make it insanely easy for devs to do the right thing</p>
+            <h3>What is it?</h3>
+            <blockquote className="fragment fade-in">
+              React components which produce UI-Library-compliant markup
+            </blockquote>
+            <Code
+              className="fragment fade-in"
+              language="html"
+              code={`
+<TextButton skin="primary" />
+
+// ✨ ⬇️
+
+<button class="textbutton textbutton-skin-primary"></button>
+            `}
+            />
+          </Slide>
+          <Slide>
+            <h3>
+              <Emoji unicodeOrShortName=":hockey:" />
+              <Emoji unicodeOrShortName=":goal:" /> Goals
+            </h3>
+            <p className="fragment fade-in">
+              Make it insanely easy for devs to do the right thing
+            </p>
             <ListWithEmojis
               animated
               items={[
                 {
-                  emojiUnicodeOrShortName: ':wheelchair:',
-                  text: 'Accessibility',
+                  emojiUnicodeOrShortName: ":wheelchair:",
+                  text: "Accessibility"
                 },
                 {
-                  emojiUnicodeOrShortName: ':iphone:',
-                  text: 'Mobile',
+                  emojiUnicodeOrShortName: ":iphone:",
+                  text: "Mobile"
                 },
                 {
-                  emojiUnicodeOrShortName: ':nail_care:',
-                  text: 'Consistent style',
-                },
+                  emojiUnicodeOrShortName: ":nail_care:",
+                  text: "Consistent style"
+                }
               ]}
             />
           </Slide>
           <Slide>
-            <h3><Emoji unicodeOrShortName=":rocket:" /> Version 1.0</h3>
-            <p className="fragment fade-in" style={{ opacity: 0.6 }}>(v1.2.2)</p>
+            <h3>
+              <Emoji unicodeOrShortName=":rocket:" /> Version 1.0
+            </h3>
+            <p className="fragment fade-in" style={{ opacity: 0.6 }}>
+              (v1.2.2)
+            </p>
             <ListWithEmojis
               animated
               items={[
                 {
-                  emojiUnicodeOrShortName: ':arrow_down:',
-                  text: 'Vistaprint Artifactory',
+                  emojiUnicodeOrShortName: ":arrow_down:",
+                  text: "Vistaprint Artifactory"
                 },
                 {
-                  emojiUnicodeOrShortName: ':book:',
-                  text: 'CoreWiki: React UI Library',
+                  emojiUnicodeOrShortName: ":book:",
+                  text: "CoreWiki: React UI Library"
                 },
                 {
-                  emojiUnicodeOrShortName: ':speech_balloon:',
-                  text: 'Slack: #proj-react-ui-library',
-                },
+                  emojiUnicodeOrShortName: ":speech_balloon:",
+                  text: "Slack: #proj-react-ui-library"
+                }
               ]}
             />
           </Slide>
@@ -110,17 +170,17 @@ class App extends React.Component {
             <ListWithEmojis
               items={[
                 {
-                  emojiUnicodeOrShortName: ':atom:',
-                  text: '#help-reactjs',
+                  emojiUnicodeOrShortName: ":atom:",
+                  text: "#help-reactjs"
                 },
                 {
-                  emojiUnicodeOrShortName: ':desktop:',
-                  text: '#sig-front-end',
+                  emojiUnicodeOrShortName: ":desktop:",
+                  text: "#sig-front-end"
                 },
                 {
-                  emojiUnicodeOrShortName: ':man_mage:',
-                  text: '#guild-front-end',
-                },
+                  emojiUnicodeOrShortName: ":man_mage:",
+                  text: "#guild-front-end"
+                }
               ]}
             />
           </Slide>
--- src/components/Code.js
+++ src/components/Code.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Highlight from 'react-highlight';
+import React from "react";
+import PropTypes from "prop-types";
+import Highlight from "react-highlight";
 
 function Code(props) {
-  const {
-    language,
-    code,
-  } = props;
+  const { language, code, ...divProps } = props;
 
   return (
-    <Highlight className={language}>
-      {code}
-    </Highlight>
+    <div {...divProps}>
+      <Highlight className={language}>{code}</Highlight>
+    </div>
   );
 }
 
 Code.propTypes = {
   language: PropTypes.string.isRequired,
-  code: PropTypes.string.isRequired,
+  code: PropTypes.string.isRequired
 };
 
 export default Code;
